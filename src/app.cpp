3421d75e6543027c9fe7704ab95173feeba4385c
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-11-11 19:14:10 +0000

    corrected compilation errors

--- src/app.cpp
+++ src/app.cpp
@@ -259,10 +259,10 @@ void App::start(){
     #endif
     #ifdef DIOGO
         //save_all();
-        list_clients();
-        list_drivers();
-        list_managers();
-        list_trucks();
+        //list_clients();
+        //list_drivers();
+        //list_managers();
+        //list_trucks();
         list_services();
     #endif
 
