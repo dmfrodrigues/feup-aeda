d77a65587fc162851b2f365ff3b8f6f9987a037f
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-11-16 02:05:09 +0000

    minor changes

--- codigo/include/app.h
+++ codigo/include/app.h
@@ -172,12 +172,14 @@ private:
      */
     static void display(const CargoTransDangerous    *p, const User::Type &t);
     /**
-     * @brief Prints detailed information about one cargo.
-     * @param p     Cargo to be displayed
+     * @brief Prints detailed information about one service.
+     * @param p     Service to be displayed
      * @param t     Type of user that information will be displayed to
      */
            void display(const Service                *p, const User::Type &t) const;
-    ///Lists
+           
+    //Lists
+
     /**
      * @brief Filters vector by type.
      * @param v Vector to be filtered
