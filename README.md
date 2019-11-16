a6eef8a67a34c2b59515b6569a51bb0c272c6023
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2019-11-16 23:35:38 +0000

    cleanups

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Transportes SML {#mainpage}
+
+Assuming you're always on feup-aeda/.
+
+## Compiling
+
+```bash
+cd codigo
+make
+```
+
+## Running
+
+```bash
+cd codigo
+./bin/main.app
+```
+
+## Cleaning up
+
+```bash
+cd codigo
+make clean
+```
--- apresentacao/process_features.sh
+++
@@ -1,5 +0,0 @@
-cd ../codigo/
-make
-./bin/main.app < ../apresentacao/feature1.in > ../apresentacao/feature1.out
-./bin/main.app < ../apresentacao/feature2.in > ../apresentacao/feature2.out
-./bin/main.app < ../apresentacao/feature3.in > ../apresentacao/feature3.out
