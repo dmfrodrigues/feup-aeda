dc7a3294bf1bf48f58f95caad79b2ee1dd17d48f
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-01-04 22:00:46 +0000

    changed zip creator

--- create_zip.sh
+++ create_zip.sh
@@ -15,7 +15,7 @@ rm -f apresentacao.snm
 rm -f apresentacao.toc
 rm -f apresentacao.vrb
 cd ..
-rm -f AEDA1920_Turma5_G3.zip
-zip -r AEDA1920_Turma5_G3.zip codigo/ documentacao/ README.md
+rm -f AEDA1920Parte2_Turma5_G3.zip
+zip -r AEDA1920Parte2_Turma5_G3.zip codigo/ documentacao/ README.md
 cd apresentacao/
-zip -r ../AEDA1920_Turma5_G3.zip apresentacao.pdf
+zip -r ../AEDA1920Parte2_Turma5_G3.zip apresentacao.pdf
